6440dd7189a78d9f1dc527e876e07001897f7008
Benjamin Day <benday@benday.com>
2024-09-18 15:37:24 -0400

    Added final check for HTTP POST tests

--- test/Benday.DemoApp.IntegrationTests/DecisionControllerFixture.cs
+++ test/Benday.DemoApp.IntegrationTests/DecisionControllerFixture.cs
@@ -32,4 +32,68 @@ public class DecisionControllerFixture
 
         Assert.True(response.IsSuccessStatusCode);
     }
+
+    [Fact]
+    public async Task Index_Post_Cool_IsCool()
+    {
+        // arrange
+        var factory = new WebApplicationFactory<JustAnEmptyClass>();
+
+        var client = factory.CreateClient();
+
+        var formValueName = "itemToCheck";
+        var formValue = "cool";
+
+        var contentToPost = new FormUrlEncodedContent(
+            new Dictionary<string, string>
+            {
+                { formValueName, formValue }
+            });
+
+        var expected = "It's cool!";
+
+        // act
+        var response = await client.PostAsync("decision/", contentToPost);
+
+        // assert
+        Assert.NotNull(response);
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        response.EnsureSuccessStatusCode();
+
+        Assert.Contains(expected, content);
+    }
+
+    [Fact]
+    public async Task Index_Post_Blah_IsNotCool()
+    {
+        // arrange
+        var factory = new WebApplicationFactory<JustAnEmptyClass>();
+
+        var client = factory.CreateClient();
+
+        var formValueName = "itemToCheck";
+        var formValue = "blah";
+
+        var contentToPost = new FormUrlEncodedContent(
+            new Dictionary<string, string>
+            {
+                { formValueName, formValue }
+            });
+
+        var expected = "It's NOT cool.";
+
+        // act
+        var response = await client.PostAsync("decision/", contentToPost);
+
+        // assert
+        Assert.NotNull(response);
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        response.EnsureSuccessStatusCode();
+
+        Assert.Contains(expected, content);
+    }
 }
